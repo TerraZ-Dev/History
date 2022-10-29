1bb00ff66c224b448226a6969608e252a97c6f5e
Julian <julianmuchacho666@gmail.com>
2022-10-29 17:15:57 -0400

    fixed for microsoft.data.sqlite

--- History/Commands/SaveCommand.cs
+++ History/Commands/SaveCommand.cs
@@ -35,7 +35,7 @@ namespace History.Commands
 							command.CommandText = "INSERT INTO History (Time, Account, Action, XY, Data, Style, Paint, WorldID, Text, Alternate, Random, Direction) VALUES (@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11)";
 							for (int i = 0; i < 12; i++)
 							{
-								command.AddParameter("@" + i, null);
+								command.AddParameter("@" + i, DBNull.Value);
 							}
 							command.Parameters[7].Value = Main.worldID;
 
@@ -71,7 +71,7 @@ namespace History.Commands
 							command.CommandText = "INSERT INTO History (Time, Account, Action, XY, Data, Style, Paint, WorldID, Text, Alternate, Random, Direction) VALUES (@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11)";
 							for (int i = 0; i < 12; i++)
 							{
-								command.AddParameter("@" + i, null);
+								command.AddParameter("@" + i, DBNull.Value);
 							}
 							command.Parameters[7].Value = Main.worldID;
 
